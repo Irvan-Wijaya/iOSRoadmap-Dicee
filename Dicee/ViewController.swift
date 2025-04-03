7982f704323798bb959e5c2c0a918331a7297e69
Irvan Wijaya <irvan.wijaya@binus.ac.id>
2025-04-03 22:04:45 +0700

    Update Main View

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewLeft.image = UIImage(named: "DiceSix")
+        diceImageViewRight.image = UIImage(named: "DiceTwo")
+        
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewLeft.image = UIImage(named: "DiceFive")
+        diceImageViewRight.image = UIImage(named: "DiceFive")
+    }
+    
 }
 
